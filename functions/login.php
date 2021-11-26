b7854166aaa08f41c8dc0c266c64299c69515e18
Lubo7 <Lkramoris@gmail.com>
2021-11-26 17:09:04 +0100

    Login style and CSS

--- functions/login.php
+++ functions/login.php
@@ -1,56 +1,59 @@
 <?php
 
-function login () {
+function login()
+{
     require '../database/db_conect.php';
     $mysqli = conectar();
 
-    if(isset($_POST['login'])){
+    if (isset($_POST['login'])) {
 
         // 3. Variables $_POST[]
         $correo = $_POST['correo'];
         $clave = $_POST['clave'];
 
-        if($correo == "" || $clave == ""){ // Validamos que ningún campo quede vacío
+        if ($correo == "" || $clave == "") { // Validamos que ningún campo quede vacío
             echo "alert('Error: correo y/o clave vacios!!');"; // Se utiliza Javascript dentro de PHP
-        }else{
+        } else {
             // 4. Cadena de SQL
             $sql = "SELECT * FROM usuario WHERE Correo ='$correo' AND Clave='$clave'";
 
             // 5. Ejecuto cadena query()
-            if(!$consulta = $mysqli->query($sql)){
+            if (!$consulta = $mysqli->query($sql)) {
                 echo "ERROR: no se pudo ejecutar la consulta!";
-            }else{
+            } else {
 
                 // 6. Cuento registros obtenidos del select. 
                 // Como el nombre de usuario en la clave primaria no debería de haber mas de un registro con el mismo nombre.
                 $filas = mysqli_num_rows($consulta);
 
                 // 7. Comparo cantidad de registros encontrados
-                if($filas == 0){
+                if ($filas == 0) {
                     echo "alert('Error: usuario y/o clave incorrectos!!');";
-                }else{
-                    $reg=$consulta->fetch_assoc();
-                    header('location: profile.php?id='.$reg['Usuario_ID']); // Si está todo correcto redirigimos a otra página
+                } else {
+                    $reg = $consulta->fetch_assoc();
+                    header('location: profile.php?id=' . $reg['Usuario_ID']); // Si está todo correcto redirigimos a otra página
                     $_SESSION['logged_in'] = true;
                 }
-
             }
         }
     }
 
-    echo 
-        '<form action="" method="post">
-            <table>
+    echo
+    '<div class="content">
+        <div class="col-lg-1">
+            <form role="form" class="custom-centered" action="" method="post">
+              <table class="form-group">
                 <tr>
-                    <td>Correo: </td>   
-                    <td><input type="email" name="correo" /></td>
+                 <td>Correo: </td>   
+                 <td><input type="email" name="correo" placeholder="nombre@ejemplo.com *" class="form-control" /></td>
                 </tr>
-                <tr>
-                    <td>Clave: </td>   
-                    <td><input type="password" name="clave" /></td>
+                <tr class="form-group">
+                 <td>Clave: </td>   
+                 <td><input type="password" name="clave" placeholder="clave *" class="form-control"  /></td>
                 </tr>
-            </table>
-        <p><input type="submit" name="login" value="Login"/></p>
-        </form>';
+               </table>
+             <p><input type="submit" name="login" value="Login" class="btn btn-lg btn-warning mt-3" /></p>
+            </form>
+        </div>
+    </div>';
 };
-?>
