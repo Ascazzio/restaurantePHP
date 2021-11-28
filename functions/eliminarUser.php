d71c063088ef655a87ba000222b839710295ae73
Lubo7 <Lkramoris@gmail.com>
2021-11-29 00:11:23 +0100

    Styles updated in eliminarUser, Platos and Users

--- functions/eliminarUser.php
+++ functions/eliminarUser.php
@@ -3,14 +3,25 @@
 require '../database/db_conect.php';
 $mysqli = conectar();
 
+
+
 if (isset($_POST['eliminar'])) {
 	$resultado = $mysqli->query("DELETE FROM usuario WHERE Usuario_ID=".$_GET["id"]);
 	header('location: users.php');
 }
 
-echo '¿Estás seguro de eliminar el user '.($_GET["nombre"]).'?
-<form action="" method="post">
-	<p><input type="submit" name="eliminar" value="Si"> 
-	<input type="submit" name="volver" value="No" onClick=window.location.replace("users.php")></p>	
-</form>'
+require_once( '../functions/header.php');
+    cabecera(); 
+	
+echo '	<link rel="stylesheet" href="../CSS/styles.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+		<div id="form-horizontal" class="mt-5">
+		¿Estás seguro de eliminar el user '.($_GET["nombre"]).'?
+		<form action="" method="post">
+			<p><input class="btn btn-sm btn-outline-warning mt-4"  type="submit" name="eliminar" value="Si"> 
+			<input class="btn btn-sm btn-outline-warning mt-4"  type="submit" name="volver" value="No" onClick=window.location.replace("users.php")></p>	
+		</form>
+		</div>'
 ?>
--- functions/platos.php
+++ functions/platos.php
@@ -1,6 +1,4 @@
-<h1>
-    Lista de Platos
-</h1>
+<h2>Lista de Platos</h2>
 
 <table>
 
@@ -10,13 +8,21 @@ require '../database/db_conect.php';
 $mysqli = conectar();
 $resultado = $mysqli->query("SELECT Plato_id, titulo FROM plato");
 
+require_once( '../functions/header.php');
+cabecera();
+
 while($reg=$resultado->fetch_assoc()){
     echo
-        '<tr>
-            <td><a href="http://localhost/devfsd/mysqli/detalle.php?id='.$reg['Plato_id'].'&titulo='.$reg['titulo'].'">'.$reg['titulo'].'</a></td>   
-            <td><input type="button" name="editar" value="Editar" onClick=window.location.replace("editar.php?id='.$reg['Plato_id'].'")>
-            <td><input type="button" name="eliminar" value="Eliminar" onClick=window.location.replace("eliminar.php?id='.$reg['Plato_id'].'&titulo='.$reg['titulo'].'")></td>
-        </tr>';
+        '<link rel="stylesheet" href="../CSS/styles.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        
+        <form class="mb-2 p-2" id="form-horizontal">
+            <tr class="">
+                <td class="col-form-label px-2"><a class="text-decoration-none" href="http://localhost/devfsd/mysqli/detalle.php?id='.$reg['Plato_id'].'&titulo='.$reg['titulo'].'">'.$reg['titulo'].'</a></td>   
+                <td class="col-form-label px-2"><input class="btn btn-sm px-2 btn-outline-warning mt-4" type="button" name="editar" value="Editar" onClick=window.location.replace("editar.php?id='.$reg['Plato_id'].'")>
+                <td class="col-form-label px-2"><input class="btn btn-sm px-2 btn-outline-warning mt-4" type="button" name="eliminar" value="Eliminar" onClick=window.location.replace("eliminar.php?id='.$reg['Plato_id'].'&titulo='.$reg['titulo'].'")></td>
+            </tr>';
 }        
 ?>
 
@@ -25,15 +31,23 @@ while($reg=$resultado->fetch_assoc()){
 <?php
 
 echo 
-    '<form action="nuevo.php" method="post">
-        <p><input type="submit" value="Nuevo plato"/></p>
-    </form>';
-echo 
-    '<form action="register.php" method="post">
-        <p><input type="submit" value="Registrar"/></p>
-    </form>';
-echo 
-    '<form action="users.php" method="post">
-        <p><input type="submit" value="UserList"/></p>
-    </form>';
+    '<div id="form-horizontal">
+        <form action="nuevo.php" method="post">
+            <p><input class="btn btn-lg btn-outline-warning mt-4" type="submit" value="Nuevo plato"/></p>
+        <form action="register.php" method="post">
+            <p><input class="btn btn-lg btn-outline-warning mt-4" type="submit" value="Registrar"/></p> 
+        <form action="users.php" method="post">
+            <p><input class="btn btn-lg btn-outline-warning mt-4" type="submit" value="UserList"/></p>
+    </div>
+
+    <footer class="p-5 bg-dark text-white text-center position-relative">
+    <div class="container">
+        <p class="lead">
+            Copyright &copy; 2021 geekshubsacademyrestaurant
+        </p>
+        <a href="" class="position-absolute bottom-0 end-0 p-5">
+        <i class="bi bi-arrow-up-circle h1"></i>
+    </a>   
+    </div>
+</footer>';
 ?>
