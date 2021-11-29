deefe8babbd54767b47a93470caba3076e0c473b
Fabrizio Pironi <fabriziopironi@gmail.com>
2021-11-29 23:41:34 +0100

    Bootstrap para detalles.php

--- functions/detalle.php
+++ functions/detalle.php
@@ -7,7 +7,12 @@ $mysqli = conectar();
 $resultado = $mysqli->query("SELECT * FROM plato WHERE id=".$_GET["id"]);
 $reg=$resultado->fetch_assoc();
 
-echo '<ul>
+echo '
+    <link rel="stylesheet" href="../CSS/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    
+    <ul>
         <li>Identificador: ' .$reg['id'].'<li>
         <li>Nombre del plato: ' .$reg['titulo'].'<li>
         <li>Numero de comensales: ' .$reg['comensales'].'<li>
